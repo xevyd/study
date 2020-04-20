0a435499b34425bcd95018e5fd6cf1c3f531f289
Alexey Chumnov <ss.moonlightpath@gmail.com>
2020-04-20 15:32:51 +0400

    fp fix

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,18 +5,19 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        res = _array.filter_map { |film| film unless film["country"].nil? }
-        res = res.filter_map { |film| film unless film["rating_kinopoisk"].nil? }
-        res = res.filter_map { |film| film if film["rating_kinopoisk"].to_f > 0}
-        res = res.filter_map { |film| film["rating_kinopoisk"].to_f if film["country"].split(',').length >= 2 }
-        return res.reduce(0, :+) { |rate| rate.to_f } / res.length
+        res = _array.filter_map { |film| film if film["country"].present? }.
+                     filter_map { |film| film if film["rating_kinopoisk"].present? }.
+                     filter_map { |film| film if film["rating_kinopoisk"].to_f > 0}.
+                     filter_map { |film| film["rating_kinopoisk"].to_f if film["country"].split(',').length >= 2 }
+        rating = res.reduce(0, :+) { |rate| rate.to_f }
+        rating / res.length
       end
 
       def chars_count(_films, _threshold)
-        res = _films.filter_map { |film| film unless film["rating_kinopoisk"].nil? }
-        res = res.filter_map { |film| film["name"] if film["rating_kinopoisk"].to_f >= _threshold }
-        res = res.map { |name| name.count('и') }
-        return res.reduce(0, :+) { |name| name }
+        res = _films.filter_map { |film| film unless film["rating_kinopoisk"].nil? }.
+              filter_map { |film| film["name"] if film["rating_kinopoisk"].to_f >= _threshold }.
+              map { |name| name.count('и') }
+        res.reduce(0, :+) { |name| name }
       end
     end
   end
